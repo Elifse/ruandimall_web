087d0ec7c25e57fd1130ec51984c0687273441c9
elifse <1574163742@qq.com>
2023-02-23 10:39:14 +0800

    modified:   src/components/Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,15 @@
+<!--
+ * @Date: 2022-12-06 14:00:27
+ * @Author: Elifse Siu
+ * @fileName: $TM_FILENAME_BASE
+ * @FilePath: \ruandimall\src\components\Footer.vue
+ * @Description: $1
+-->
 // 页脚
 <template>
   <div>
     <el-footer style="text-align: center; background-color: #ffffff; color: black;">
-      <span>Design by Elifse Siu 版权所有 © 2001-2022</span></br>
+      <span>Design by Elifse Siu 版权所有 © 2001-2022</span>
       <el-link target="_blank" type="info" href="http://elifse.cloud" >了解更多</el-link> |
       <el-link target="_blank" type="primary" href="https://beian.miit.gov.cn/" >鄂ICP备2022008047号</el-link>
     </el-footer>
